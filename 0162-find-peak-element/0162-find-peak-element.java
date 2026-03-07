c870cee8c68cb4b6ec236981ea9cba43d3199acb
Shashank M <99338637+Shashankmanju@users.noreply.github.com>
2026-03-07 12:48:10 +0530

    Time: 0 ms (100%), Space: 44 MB (94.36%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -5,12 +5,13 @@ class Solution {
 
         while(left<right){
             int mid=(left+right)/2;
+
             if(nums[mid]<nums[mid+1]){
                 left=mid+1;
             }else{
                 right=mid;
             }
         }
-        return right;
+        return left;
     }
 }
