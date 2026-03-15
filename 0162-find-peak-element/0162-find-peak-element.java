c81865c39de00844dfadfd74a915e98f938ff2e8
Shashank M <99338637+Shashankmanju@users.noreply.github.com>
2026-03-15 09:43:37 +0530

    Time: 0 ms (100%), Space: 44 MB (93.16%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,7 +1,7 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int left=0;
-        int right=nums.length-1;
+       int left=0;
+       int right=nums.length-1;
 
         while(left<right){
             int mid=(left+right)/2;
@@ -12,6 +12,6 @@ class Solution {
                 right=mid;
             }
         }
-        return right;
+        return left;
     }
 }
